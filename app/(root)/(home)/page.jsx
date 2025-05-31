05fc8fca8f02c4042f4406c9179cfb607e98dc41
Arezoo <skyai.company@gmail.com>
2025-06-01 00:26:44 +0330

    second

--- app/(root)/(home)/page.jsx
+++ app/(root)/(home)/page.jsx
@@ -1,5 +1,5 @@
 import LuxuryCarSelection from '@/components/carselection'
-import ContactUs from '@/components/contactus'
+import ContactUs from '@/components/ContactUs'
 import Hero from '@/components/hero'
 import CustomerReviews from '@/components/reviews'
 import WhyChooseUs from '@/components/whyus'

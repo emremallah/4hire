fd7b271202b0e83dffc5a09e7da4d3be3c8fd79a
Arezoo <skyai.company@gmail.com>
2025-06-06 02:08:22 +0330

    fix cars variable

---
+++ components/cars.jsx
@@ -0,0 +1,99 @@
+const cars = [
+  {
+    id: "1",
+    name: "Lamborghini Huracán",
+    price: "$899/day",
+    availability: true,
+    deposit: true,
+    transmission: "Automatic",
+    seats: 2,
+    luggage: 1,
+    fuel: "Gasoline",
+    images: ["/images/lamborghini.jpg"],
+    description:
+      "The Lamborghini Huracán combines incredible performance with a sleek and aggressive design.",
+  },
+  {
+    id: "2",
+    name: "Rolls Royce Phantom",
+    price: "$1299/day",
+    availability: false,
+    deposit: true,
+    transmission: "Automatic",
+    seats: 5,
+    luggage: 3,
+    fuel: "Gasoline",
+    images: ["/images/rollsroyce.jpg"],
+    description:
+      "Experience ultimate luxury with the Rolls Royce Phantom. Unmatched comfort, elegance, and status.",
+  },
+  {
+    id: "ferrari",
+    name: "Ferrari 458",
+    price: "$800/day",
+    availability: true,
+    deposit: true,
+    transmission: "Automatic",
+    seats: 5,
+    luggage: 3,
+    fuel: "Gasoline",
+    images: ["/images/ferrari.jpg"],
+    description:
+      "Feel the thrill of the iconic Ferrari 458 with razor-sharp handling and breathtaking acceleration.",
+  },
+  {
+    id: "mustang",
+    name: "Ford Mustang 2024",
+    price: "AED450/day",
+    availability: true,
+    deposit: true,
+    transmission: "Automatic",
+    seats: 4,
+    luggage: 3,
+    fuel: "Gasoline",
+    images: [
+      "/images/mustang/mustang.jpeg",
+      "/images/mustang/mustang2.jpeg",
+    ],
+    description:
+      "The 2024 Mustang Ecoboost offers modern muscle with efficiency—perfect for cruising Dubai’s highways.",
+  },
+  {
+    id: "dodge-challenger",
+    name: "Dodge Challenger 2023",
+    price: "AED450/day",
+    availability: true,
+    deposit: true,
+    transmission: "Automatic",
+    seats: 4,
+    luggage: 3,
+    fuel: "Gasoline",
+    images: ["/images/dodge-challenger/1.jpeg" , "/images/dodge-challenger/2.jpeg" , "/images/dodge-challenger/3.jpeg" , 
+      "/images/dodge-challenger/4.jpeg" , "/images/dodge-challenger/5.jpeg" , "/images/dodge-challenger/6.jpeg", "/images/dodge-challenger/7.jpeg"
+    ],
+    description:
+      "Drive the Ford Mustang 2024 and feel the roar of a V4 ecoboost with sporty performance.",
+  },
+  {
+    id: "patrol-2023",
+    name: "Patrol 2023",
+    price: "AED400/day",
+    availability: true,
+    deposit: true,
+    transmission: "Automatic",
+    seats: 4,
+    luggage: 3,
+    fuel: "Gasoline",
+    images: [
+      "/images/nismo/1.jpg",
+      "/images/nismo/2.jpg",
+      "/images/nismo/3.jpg",
+      "/images/nismo/4.jpg",
+      "/images/nismo/5.jpg"
+    ],
+    description:
+      "The 2024 Mustang Ecoboost offers modern muscle with efficiency—perfect for cruising Dubai’s highways.",
+  },
+];
+
+export default cars;
